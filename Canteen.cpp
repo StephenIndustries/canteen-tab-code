7775a4fb530aa1672aa920b17509f003c2e0588a
StephenIndustries <33757254+StephenIndustries@users.noreply.github.com>
2018-05-07 15:30:18 -0600

    Update Canteen.cpp

--- Canteen.cpp
+++ Canteen.cpp
@@ -1,3 +1,15 @@
 int main() {
   return 0;
   }
+#include <sstream>
+#include <string>
+
+std::string line;
+while (std::getline(infile, line))
+{
+    std::istringstream iss(line);
+    int a, b;
+    if (!(iss >> a >> b)) { break; } // error
+
+    // process pair (a,b)
+}  
